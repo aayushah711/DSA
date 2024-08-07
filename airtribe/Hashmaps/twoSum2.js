8e17c5c949593d5e923b40c407bc42b9c379222c
Aayushi Shah <aayu.shah711@gmail.com>
2024-08-07 12:00:49 +0530

    Two Sum II

--- airtribe/Hashmaps/twoSum2.js
+++ airtribe/Hashmaps/twoSum2.js
@@ -4,16 +4,19 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let obj = {};
-  for (let i = 0; i < nums.length; i++) {
-    let num = nums[i];
-    if (obj[target - num] >= 0) {
-      return [obj[target - num] + 1, i + 1];
+  let r = nums.length - 1;
+  let l = 0;
+  while (l < r) {
+    let sum = nums[l] + nums[r];
+    if (sum < target) {
+      l++;
+    } else if (sum > target) {
+      r--;
+    } else {
+      return [l + 1, r + 1];
     }
-
-    obj[num] = i;
   }
 };
-console.log(twoSum([2, 7, 11, 15], 9)); // [1,2]
-console.log(twoSum([3, 2, 4], 6)); // [2,3]
-console.log(twoSum([3, 3], 6)); // [1,2]
+// console.log(twoSum([2, 7, 11, 15], 9)); // [1,2]
+// console.log(twoSum([3, 2, 4], 6)); // [2,3]
+// console.log(twoSum([3, 3], 6)); // [1,2]
