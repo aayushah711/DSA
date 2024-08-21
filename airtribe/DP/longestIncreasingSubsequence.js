9b75db263983e71cd4ad67ab6334fc66fddb0553
Aayushi Shah <aayu.shah711@gmail.com>
2024-08-21 17:00:11 +0530

    Patience sort

--- airtribe/DP/longestIncreasingSubsequence.js
+++ airtribe/DP/longestIncreasingSubsequence.js
@@ -4,19 +4,49 @@
  */
 var lengthOfLIS = function (nums) {
   let n = nums.length;
-  let dp = new Array(n).fill(1);
-  for (let i = n - 1; i >= 0; i--) {
-    for (let j = i; j < n; j++) {
-      if (nums[i] < nums[j]) {
-        dp[i] = Math.max(dp[i], dp[j] + 1);
-      }
+  let piles = [nums[0]];
+
+  function binarySearch(key) {
+    let left = 0;
+    let right = piles.length;
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+      if (piles[mid] === key) return mid;
+      if (piles[mid] > key) {
+        right = mid;
+      } else left = mid + 1;
+    }
+    return right;
+  }
+
+  for (let i = 1; i < n; i++) {
+    let num = nums[i];
+
+    let idx = binarySearch(num);
+    if (idx === piles.length) {
+      piles.push(num);
+    } else {
+      piles[idx] = num;
     }
   }
-  return Math.max(...dp);
+
+  // TC O(NlogN)
+  // SC O(N)
+
+  return piles.length;
+  // let dp = new Array(n).fill(1);
+  // for (let i = n - 1; i >= 0; i--) {
+  //   for (let j = i; j < n; j++) {
+  //     if (nums[i] < nums[j]) {
+  //       dp[i] = Math.max(dp[i], dp[j] + 1);
+  //     }
+  //   }
+  // }
+  // return Math.max(...dp);
+
+  // TC O(N^2)
+  // SC O(N)
 };
 console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18])); //4
 console.log(lengthOfLIS([0, 1, 0, 3, 2, 3])); //4
 console.log(lengthOfLIS([7, 7, 7, 7, 7, 7, 7])); //1
-
-// TC O(N^2)
-// SC O(N)
