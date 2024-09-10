b38ae6d5bf162f6e416dce554a6bfb735e9ffb18
Aayushi Shah <aayu.shah711@gmail.com>
2024-09-10 14:18:10 +0530

    check visited set

--- airtribe/Graphs/dijkstra.js
+++ airtribe/Graphs/dijkstra.js
@@ -1,9 +1,18 @@
+// This algorithm can further be optimised by using min heap / Priority queue
 // Define a graph using an adjacency list
+// const graph = {
+//   A: { B: 1, C: 4 }, // Node A is connected to Node B with a weight of 1 and Node C with a weight of 4
+//   B: { A: 1, C: 2, D: 5 }, // ... and so on for other nodes
+//   C: { A: 4, B: 2, D: 1 },
+//   D: { B: 5, C: 1 },
+// };
 const graph = {
-  A: { B: 1, C: 4 }, // Node A is connected to Node B with a weight of 1 and Node C with a weight of 4
-  B: { A: 1, C: 2, D: 5 }, // ... and so on for other nodes
-  C: { A: 4, B: 2, D: 1 },
-  D: { B: 5, C: 1 },
+  A: { B: 2, C: 4 }, // Node A is connected to Node B with a weight of 1 and Node C with a weight of 4
+  B: { C: 1, D: 7 }, // ... and so on for other nodes
+  C: { E: 3 },
+  D: { F: 1 },
+  E: { D: 2, F: 5 },
+  F: {},
 };
 
 function dijkstra(graph, start) {
@@ -19,9 +28,11 @@ function dijkstra(graph, start) {
     nodes.sort((a, b) => distances[a] - distances[b]);
     let node = nodes.shift();
     if (distances[node] === Infinity) continue;
-    if (node) {
-      let neighbors = graph[node];
-      for (let neighbor in neighbors) {
+    let neighbors = graph[node];
+    visited.add(node);
+
+    for (let neighbor in neighbors) {
+      if (!visited.has(neighbor)) {
         distances[neighbor] = Math.min(
           distances[neighbor],
           distances[node] + neighbors[neighbor]
@@ -35,3 +46,4 @@ function dijkstra(graph, start) {
 
 // Example: Find shortest distances from node A to all other nodes in the graph
 console.log(dijkstra(graph, "A")); // Outputs: { A: 0, B: 1, C: 3, D: 4 }
+// { A: 0, B: 2, C: 3, D: 8, E: 6, F: 9 };
